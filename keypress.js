5e30ebaa500f33028ed33bfed085a3a6c975d9f4
Satyam <satyamworks20@gmail.com>
2025-10-07 11:18:19 +0530

    File changes added

---
+++ keypress.js
@@ -0,0 +1,18 @@
+const ks = require('node-key-sender');
+
+console.log('Starting script in 5 seconds...');
+
+setTimeout(() => {
+  console.log('Sending ENTER key...');
+  
+  ks.sendKey('enter');
+
+  setTimeout(() => {
+    console.log('Sending CTRL+TAB...');
+    
+    ks.sendCombination(['control', 'tab']);
+    
+    console.log('Script finished.');
+  }, 100); 
+
+}, 5000); 
